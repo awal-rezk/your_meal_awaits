c264ac40c8a496de527a6b7c72d90f4787fc13ab
awal-rezk <awalx003@umn.edu>
2021-08-29 20:48:43 -0500

    Adding app files

---
+++ DinerApplication/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace DinerApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.backgroundImage1 = new System.Windows.Forms.PictureBox();
+            this.welcome_txt_box = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundImage1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnStart.Image = ((System.Drawing.Image)(resources.GetObject("btnStart.Image")));
+            this.btnStart.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnStart.Location = new System.Drawing.Point(333, 334);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(127, 66);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnClickThis_Click);
+            // 
+            // backgroundImage1
+            // 
+            this.backgroundImage1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.backgroundImage1.Image = ((System.Drawing.Image)(resources.GetObject("backgroundImage1.Image")));
+            this.backgroundImage1.Location = new System.Drawing.Point(0, 0);
+            this.backgroundImage1.Name = "backgroundImage1";
+            this.backgroundImage1.Size = new System.Drawing.Size(800, 450);
+            this.backgroundImage1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.backgroundImage1.TabIndex = 1;
+            this.backgroundImage1.TabStop = false;
+            this.backgroundImage1.Click += new System.EventHandler(this.backgroundImage1_Click);
+            // 
+            // welcome_txt_box
+            // 
+            this.welcome_txt_box.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.welcome_txt_box.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.welcome_txt_box.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.welcome_txt_box.ForeColor = System.Drawing.Color.LightCoral;
+            this.welcome_txt_box.Location = new System.Drawing.Point(0, 0);
+            this.welcome_txt_box.Name = "welcome_txt_box";
+            this.welcome_txt_box.Size = new System.Drawing.Size(800, 13);
+            this.welcome_txt_box.TabIndex = 2;
+            this.welcome_txt_box.Text = "Welcome To Our Diner!";
+            this.welcome_txt_box.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.welcome_txt_box.TextChanged += new System.EventHandler(this.welcome_txt_box_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.welcome_txt_box);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.backgroundImage1);
+            this.Name = "Form1";
+            this.Text = "Your Meal Awaits";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundImage1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox backgroundImage1;
+        private System.Windows.Forms.TextBox welcome_txt_box;
+    }
+}
+
---
+++ DinerApplication/Form2.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace DinerApplication
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.breakfastBtn = new System.Windows.Forms.Button();
+            this.LunchBtn = new System.Windows.Forms.Button();
+            this.dinnerBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.homeBtn2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.homeBtn1 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.homeBtn3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // breakfastBtn
+            // 
+            this.breakfastBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.breakfastBtn.Image = ((System.Drawing.Image)(resources.GetObject("breakfastBtn.Image")));
+            this.breakfastBtn.Location = new System.Drawing.Point(86, 309);
+            this.breakfastBtn.Name = "breakfastBtn";
+            this.breakfastBtn.Size = new System.Drawing.Size(126, 62);
+            this.breakfastBtn.TabIndex = 0;
+            this.breakfastBtn.UseVisualStyleBackColor = true;
+            this.breakfastBtn.Click += new System.EventHandler(this.breakfastBtn_Click);
+            // 
+            // LunchBtn
+            // 
+            this.LunchBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.LunchBtn.Image = ((System.Drawing.Image)(resources.GetObject("LunchBtn.Image")));
+            this.LunchBtn.Location = new System.Drawing.Point(322, 309);
+            this.LunchBtn.Name = "LunchBtn";
+            this.LunchBtn.Size = new System.Drawing.Size(126, 62);
+            this.LunchBtn.TabIndex = 1;
+            this.LunchBtn.UseVisualStyleBackColor = true;
+            this.LunchBtn.Click += new System.EventHandler(this.LunchBtn_Click);
+            // 
+            // dinnerBtn
+            // 
+            this.dinnerBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.dinnerBtn.Image = ((System.Drawing.Image)(resources.GetObject("dinnerBtn.Image")));
+            this.dinnerBtn.Location = new System.Drawing.Point(566, 309);
+            this.dinnerBtn.Name = "dinnerBtn";
+            this.dinnerBtn.Size = new System.Drawing.Size(126, 62);
+            this.dinnerBtn.TabIndex = 2;
+            this.dinnerBtn.UseVisualStyleBackColor = true;
+            this.dinnerBtn.Click += new System.EventHandler(this.dinnerBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel3.Controls.Add(this.homeBtn2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 450);
+            this.panel3.TabIndex = 0;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // homeBtn2
+            // 
+            this.homeBtn2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.homeBtn2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("homeBtn2.BackgroundImage")));
+            this.homeBtn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.homeBtn2.Location = new System.Drawing.Point(18, 348);
+            this.homeBtn2.Name = "homeBtn2";
+            this.homeBtn2.Size = new System.Drawing.Size(50, 63);
+            this.homeBtn2.TabIndex = 2;
+            this.homeBtn2.UseVisualStyleBackColor = true;
+            this.homeBtn2.Click += new System.EventHandler(this.homeBtn2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.homeBtn1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 450);
+            this.panel2.TabIndex = 0;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // homeBtn1
+            // 
+            this.homeBtn1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.homeBtn1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("homeBtn1.BackgroundImage")));
+            this.homeBtn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.homeBtn1.Location = new System.Drawing.Point(18, 348);
+            this.homeBtn1.Name = "homeBtn1";
+            this.homeBtn1.Size = new System.Drawing.Size(50, 63);
+            this.homeBtn1.TabIndex = 1;
+            this.homeBtn1.UseVisualStyleBackColor = true;
+            this.homeBtn1.Click += new System.EventHandler(this.homeBtn1_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel4.BackgroundImage")));
+            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel4.Controls.Add(this.homeBtn3);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(800, 450);
+            this.panel4.TabIndex = 0;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // homeBtn3
+            // 
+            this.homeBtn3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.homeBtn3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("homeBtn3.BackgroundImage")));
+            this.homeBtn3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.homeBtn3.Location = new System.Drawing.Point(18, 348);
+            this.homeBtn3.Name = "homeBtn3";
+            this.homeBtn3.Size = new System.Drawing.Size(50, 63);
+            this.homeBtn3.TabIndex = 2;
+            this.homeBtn3.UseVisualStyleBackColor = true;
+            this.homeBtn3.Click += new System.EventHandler(this.homeBtn3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.breakfastBtn);
+            this.Controls.Add(this.LunchBtn);
+            this.Controls.Add(this.dinnerBtn);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "Choose a Meal";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+            this.Load += new System.EventHandler(this.Form2_);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button breakfastBtn;
+        private System.Windows.Forms.Button LunchBtn;
+        private System.Windows.Forms.Button dinnerBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button homeBtn3;
+        private System.Windows.Forms.Button homeBtn2;
+        private System.Windows.Forms.Button homeBtn1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
